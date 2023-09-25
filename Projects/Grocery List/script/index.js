0fcb21294fc116e1569d5508c83c24193b0e8175
“RavidDavda” <ravid521@gmail.com>
2023-09-26 00:51:40 +0300

    final

--- Projects/Grocery List/script/index.js
+++ Projects/Grocery List/script/index.js
@@ -1,38 +1,45 @@
 const itemsContainer = document.querySelector('.items-container');
 const cartInfo = document.querySelector('#cartInfo');
 const list = document.querySelector('#list');
-let cartCost = [];
+const loadCartBtn = document.querySelector('#loadCartBtn');
 let cartItems = [];
 
-const addNewItem = () => {
-  const { value: quantity } = document.querySelector('#quantity');
-  const { value: newItemPrice } = document.querySelector('#price');
-  const { value: listItem } = document.querySelector('#listItem');
-  if (!listItem) {
+const addNewItem = (quantity, newItemPrice, itemName) => {
+  if (!itemName) {
     return;
   } else if (!newItemPrice) {
     return;
   } else if (!quantity) {
     return;
   } else {
+    const deleteBtn = document.createElement('button');
+    deleteBtn.classList.add('itemRemove');
+    deleteBtn.textContent = 'X';
+    deleteBtn.addEventListener('click', removeItem);
     const newItem = document.createElement('li');
-    cartItems.push(newItem);
-    cartCost.push(newItemPrice * +quantity);
-    newItem.innerHTML = `<p>${quantity}</p> <p>${listItem}</p> <p>${newItemPrice}₪</p>`;
-    for (let i = 0; i < cartItems.length; i++) {
-      cartItems[i].addEventListener('click', () => {
-        cartItems[i].style.textDecoration = 'line-through';
-      })
-    };
-    document.querySelector('#list').appendChild(newItem);
+    cartItems.push({
+      quantity,
+      itemName,
+      newItemPrice,
+    });
+    localStorage.setItem('cart', JSON.stringify(cartItems))
+    newItem.innerHTML = `<p>${quantity}</p> <p>${itemName}</p> <p>${newItemPrice}₪</p>`;
+    newItem.appendChild(deleteBtn);
+    list.appendChild(newItem);
   }
+};
+
+const removeItem = (e) => {
+  e.target.parentElement.remove();
+  cartItems.splice(cartItems.indexOf(e), 1);
+  totalCost();
 };
 
 const totalCost = () => {
   let cost = document.querySelector('.totalCost');
   let totalCost = 0;
-  for (const item of cartCost) {
-    totalCost = totalCost + +item;
+  for (const item of cartItems) {
+    totalCost = totalCost + +item.newItemPrice * item.quantity;
   }
   cost.innerText = `Total Cost: ${totalCost.toFixed(2)}₪`;
   cartInfo.appendChild(cost);
@@ -40,21 +47,52 @@ const totalCost = () => {
 
 const resetCart = () => {
   document.querySelector('#resetCartBtn').addEventListener('click', () => {
-    console.log('sdaf');
     while (list.hasChildNodes()) {
       list.removeChild(list.firstChild);
     }
     cartItems = [];
-    cartCost = [];
     totalCost();
+  });
+};
+
+const loadCart = () => {
+  cartItems = [];
+  let newCartItems = [];
+  let jsonStr = localStorage.getItem('cart');
+  if (!jsonStr) {
+    return;
+  } else {
+    newCartItems = JSON.parse(jsonStr);
+    for (let item of newCartItems) {
+      addNewItem(
+        item.quantity,
+        item.newItemPrice,
+        item.itemName,
+      );
+    }
+  }
+  totalCost();
+};
+
+const handleLoadCart = () => {
+  loadCartBtn.addEventListener('click', () => {
+    loadCart();
   });
 };
 
 window.addEventListener('load', () => {
   document.querySelector('#cart').addEventListener('submit', (e) => {
     e.preventDefault();
-    addNewItem();
+    const quantity = document.querySelector('#quantity');
+    const newItemPrice = document.querySelector('#price');
+    const itemName = document.querySelector('#itemName');
+    addNewItem(
+      quantity.value,
+      newItemPrice.value,
+      itemName.value,
+    );
     totalCost();
   });
+  handleLoadCart();
   resetCart();
 });
